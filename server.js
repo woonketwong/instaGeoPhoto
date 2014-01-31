f4576fa230f1dab0058bc26bebbc7cda087e74ec
Woon Ket Wong <woonketwong@hotmail.com>
2014-02-01 02:22:05 +0800

    removed model path

--- server.js
+++ server.js
@@ -16,24 +16,6 @@ var express = require('express'),
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     config = require('./config/config')
 
-//Bootstrap models
-var models_path = __dirname + '/server/app/models';
-var walk = function(path) {
-    fs.readdirSync(path).forEach(function(file) {
-        var newPath = path + '/' + file;
-        var stat = fs.statSync(newPath);
-        if (stat.isFile()) {
-            if (/(.*)\.(js$|coffee$)/.test(file)) {
-                require(newPath);
-            }
-        } else if (stat.isDirectory()) {
-            walk(newPath);
-        }
-    });
-};
-walk(models_path);
-
-
 var app = express();
 
 console.log("****************************");
