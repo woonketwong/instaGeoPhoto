ae92c48b7d983916e0fedafc94c2dbcc6ca15967
Woon Ket Wong <woonketwong@hotmail.com>
2014-02-07 01:01:17 +0800

    remove ip input

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ var port = process.env.PORT || 5000;
 var ip = process.env.IP || "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+server.listen(port);
