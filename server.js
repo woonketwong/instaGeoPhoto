6429662fe244ca8670fb1448cef16222ef557a8b
Woon Ket Wong <woonketwong@hotmail.com>
2014-02-07 01:07:57 +0800

    delete console message

--- server.js
+++ server.js
@@ -3,5 +3,4 @@ var handler = require("./server/app/controllers/index");
 
 var port = process.env.PORT || 5000;
 var server = http.createServer(handler.handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
 server.listen(port);
