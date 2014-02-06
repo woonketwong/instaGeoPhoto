44042704fcb23e38dfd205f1dc4f018a5c028d51
Woon Ket Wong <woonketwong@hotmail.com>
2014-02-07 01:03:46 +0800

    remove express in package.json and ip argument

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var http = require("http");
 var handler = require("./server/app/controllers/index");
 
 var port = process.env.PORT || 5000;
-var ip = process.env.IP || "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port);
